943bac407c8654fbbf7d028cfbb0806b0bf067b4
Peter Vestlin <peter.vestlin@lm.se>
2019-11-11 14:24:20 +0100

    Coroutines

--- client/src/main/kotlin/nu/westlin/ticker/client/ClientApplication.kt
+++ client/src/main/kotlin/nu/westlin/ticker/client/ClientApplication.kt
@@ -1,5 +1,7 @@
 package nu.westlin.ticker.client
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import nu.westlin.ticker.model.Stock
 import org.slf4j.LoggerFactory
@@ -12,6 +14,7 @@ import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.awaitExchange
+import kotlin.system.measureTimeMillis
 
 // TODO: Ktor client instead of Spring Kofu?
 
@@ -37,7 +40,6 @@ val app = application(WebApplicationType.NONE) {
 
 data class ClientProperties(val servers: Map<String, String>)
 
-
 class Client(private val properties: ClientProperties) {
     companion object {
         private val logger = LoggerFactory.getLogger(Client::class.java)
@@ -45,15 +47,33 @@ class Client(private val properties: ClientProperties) {
 
     private val webClient = WebClient.create()
 
-    // TODO petves: Coroutines and async (map to new list)
     fun printStocks() = runBlocking {
         // TODO petves: Only show stocks that have changed it's price and show the diff with +/-
         // TODO: On startup, save url and result of GET url/stockType in an object for later display. Then change map to list for servers in application.yml
-        properties.servers.forEach { (type, url) ->
-            val stocks = webClient.get().uri(url).accept(MediaType.APPLICATION_JSON)
-                .awaitExchange().awaitBody<List<Stock>>()
-            logger.debug("Stocks of type $type:")
-            stocks.forEach { logger.debug(it.toString()) }
+        val execTime = measureTimeMillis {
+            properties.servers.map { (type, url) ->
+                async(Dispatchers.IO) {
+                    logger.debug("GET url: $url")
+                    // TODO: Use class, not Pair
+                    Pair(type, webClient.get().uri(url).accept(MediaType.APPLICATION_JSON)
+                        .awaitExchange().awaitBody<List<Stock>>())
+                }
+            }.forEach { deferred ->
+                val pair = deferred.await()
+                logger.debug("Stocks of type ${pair.first}:")
+                pair.second.forEach { logger.debug(it.toString()) }
+            }
         }
+/*
+        val execTime = measureTimeMillis {
+            properties.servers.forEach { (type, url) ->
+                val stocks = webClient.get().uri(url).accept(MediaType.APPLICATION_JSON)
+                    .awaitExchange().awaitBody<List<Stock>>()
+                logger.info("Stocks of type $type:")
+                stocks.forEach { logger.info(it.toString()) }
+            }
+        }
+*/
+        logger.debug("Exec time: $execTime ms")
     }
 }
